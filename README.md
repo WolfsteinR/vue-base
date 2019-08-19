919d9ed155e4899bb0a745b674f2377c6343fb22
wolfsz <wolfsz@yandex.ru>
2019-08-19 21:36:34 +0300

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+# Vue 2.0 Base
+
+> Base simple Vue project with axios, vuex and bootstrap 4.
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
---
+++ src/layouts/Main.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <v-link class="nav-link" href="/">Home</v-link>
+        </li>
+      </ul>
+    </nav>
+    <div class="container">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+  import VLink from '../components/VLink.vue'
+
+  export default {
+    components: {
+      VLink
+    }
+  }
+</script>
